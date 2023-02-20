aae66fcb61c33d9e497285a3db46fb3c59e536a7
vultorio <68811816+vultorio67@users.noreply.github.com>
2023-02-20 16:54:16 +0100

    update

--- src/main/java/capitaliul/capitalium/command/CommandKit.java
+++ src/main/java/capitaliul/capitalium/command/CommandKit.java
@@ -18,15 +18,6 @@ public class CommandKit implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            // Create a new ItemStack (type: diamond)
-            ItemStack diamond = new ItemStack(Material.DIAMOND);
-
-            // Create a new ItemStack (type: brick)
-            ItemStack bricks = new ItemStack(Material.BRICK);
-
-            // Set the amount of the ItemStack
-            bricks.setAmount(20);
-
             // Give the player our items (comma-seperated list of all ItemStack)
             player.getInventory().addItem(gethammer());
         }
--- src/main/java/capitaliul/capitalium/event/OnMine.java
+++ src/main/java/capitaliul/capitalium/event/OnMine.java
@@ -1,6 +1,7 @@
 package capitaliul.capitalium.event;
 
 import capitaliul.capitalium.Capitalium;
+import capitaliul.capitalium.util.BlockList;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Damageable;
@@ -25,46 +26,51 @@ public class OnMine implements Listener {
     public void updateOnInteract(PlayerInteractEvent event) {
         Player p = event.getPlayer();
         if (event.getAction() == Action.LEFT_CLICK_BLOCK && !p.isSneaking()) {
-            Block centerBlock = event.getClickedBlock();
-            double xOffset = 0.5D;
-            double yOffset = 0.5D;
-            double zOffset = 0.5D;
-            if (true) {
-                BlockFace bf = getBlockFace(p);
-                Block[] blocks = getBlocks(bf, centerBlock);
-                if (bf == BlockFace.UP || bf == BlockFace.DOWN)
-                    if (bf == BlockFace.UP) {
-                        yOffset += 0.6D;
-                    } else {
-                        yOffset -= 0.6D;
+            if ((p.getGameMode() == GameMode.SURVIVAL) &&
+                    p.getItemInHand().getType() == Material.GOLDEN_PICKAXE &&
+                    p.getItemInHand().getItemMeta().hasDisplayName() &&
+                    p.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("hammer")) {
+                Block centerBlock = event.getClickedBlock();
+                double xOffset = 0.5D;
+                double yOffset = 0.5D;
+                double zOffset = 0.5D;
+                if (true) {
+                    BlockFace bf = getBlockFace(p);
+                    Block[] blocks = getBlocks(bf, centerBlock);
+                    if (bf == BlockFace.UP || bf == BlockFace.DOWN)
+                        if (bf == BlockFace.UP) {
+                            yOffset += 0.6D;
+                        } else {
+                            yOffset -= 0.6D;
+                        }
+                    if (bf == BlockFace.NORTH || bf == BlockFace.SOUTH)
+                        if (bf == BlockFace.SOUTH) {
+                            zOffset += 0.6D;
+                        } else {
+                            zOffset -= 0.6D;
+                        }
+                    if (bf == BlockFace.EAST || bf == BlockFace.WEST)
+                        if (bf == BlockFace.EAST) {
+                            xOffset += 0.6D;
+                        } else {
+                            xOffset -= 0.6D;
+                        }
+                    byte b;
+                    int i;
+                    Block[] arrayOfBlock1;
+                    for (i = (arrayOfBlock1 = blocks).length, b = 0; b < i; ) {
+                        Block block = arrayOfBlock1[b];
+                        ItemStack pick = p.getInventory().getItemInMainHand();
+                        if (block != null && block.isPreferredTool(pick)) {
+                            Location bLoc = block.getLocation();
+                            bLoc.setX(bLoc.getX() + xOffset);
+                            bLoc.setY(bLoc.getY() + yOffset);
+                            bLoc.setZ(bLoc.getZ() + zOffset);
+                            if (true)
+                                bLoc.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, bLoc, 1);
+                        }
+                        b++;
                     }
-                if (bf == BlockFace.NORTH || bf == BlockFace.SOUTH)
-                    if (bf == BlockFace.SOUTH) {
-                        zOffset += 0.6D;
-                    } else {
-                        zOffset -= 0.6D;
-                    }
-                if (bf == BlockFace.EAST || bf == BlockFace.WEST)
-                    if (bf == BlockFace.EAST) {
-                        xOffset += 0.6D;
-                    } else {
-                        xOffset -= 0.6D;
-                    }
-                byte b;
-                int i;
-                Block[] arrayOfBlock1;
-                for (i = (arrayOfBlock1 = blocks).length, b = 0; b < i; ) {
-                    Block block = arrayOfBlock1[b];
-                    ItemStack pick = p.getInventory().getItemInMainHand();
-                    if (block != null && block.isPreferredTool(pick)) {
-                        Location bLoc = block.getLocation();
-                        bLoc.setX(bLoc.getX() + xOffset);
-                        bLoc.setY(bLoc.getY() + yOffset);
-                        bLoc.setZ(bLoc.getZ() + zOffset);
-                        if (true)
-                            bLoc.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, bLoc, 1);
-                    }
-                    b++;
                 }
             }
         }
@@ -74,8 +80,13 @@ public class OnMine implements Listener {
     public void onMine(BlockBreakEvent event) {
         Player player = event.getPlayer();
         int[] blockallowed = { 1, 14, 15, 16, 21, 56, 73, 74, 129 };
-        if (true) {
-            System.out.println("miue");
+
+        if(isPickaxe(player))
+
+        if ((player.getGameMode() == GameMode.SURVIVAL) &&
+                player.getItemInHand().getType() == Material.GOLDEN_PICKAXE &&
+                player.getItemInHand().getItemMeta().hasDisplayName() &&
+                player.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("hammer")) {
 
             Block centerBlock = event.getBlock();
             BlockFace bf = getBlockFace(player);
@@ -86,13 +97,15 @@ public class OnMine implements Listener {
             for (i = (arrayOfBlock1 = blocks).length, b = 0; b < i; ) {
                 Block block = arrayOfBlock1[b];
                 ItemStack pick = player.getInventory().getItemInMainHand();
-                if (true)
-                    if (true) {
-                        if (true) {
+                if (block != null && block.isPreferredTool(pick) && isRightToolForTheJob(block, "PICKAXE"))
+                    System.out.println("miue1");
+                    if (pick.getItemMeta() instanceof Damageable) {
+                        System.out.println("miue2");
+                        Damageable pickDamage = (Damageable) pick.getItemMeta();
+                        if (pick.getType().getMaxDurability() > pickDamage.getHealth()) {
                             System.out.printf("mia");
-                           // pickDamage.damage(pickDamage.getHealth() + 1);
+                            pickDamage.damage(pickDamage.getHealth() + 9);
                             block.breakNaturally(pick);
-                            //pick.setItemMeta((ItemMeta)pickDamage);
                         }
                     }
                 b++;
@@ -153,5 +166,34 @@ public class OnMine implements Listener {
         }
         return blocks;
     }
+
+
+    public boolean isRightToolForTheJob(Block block, String toolGroup) {
+        String tool = BlockList.valueOf(block.getType().toString()).getTool();
+        if (tool.equals(toolGroup))
+            return true;
+        return false;
+    }
+
+    public boolean isPickaxe(Player p) {
+        ItemStack mainHand = p.getInventory().getItemInMainHand();
+        Material[] picks = { Material.WOODEN_PICKAXE,
+                Material.STONE_PICKAXE,
+                Material.IRON_PICKAXE,
+                Material.GOLDEN_PICKAXE,
+                Material.DIAMOND_PICKAXE,
+                Material.NETHERITE_PICKAXE };
+        byte b;
+        int i;
+        Material[] arrayOfMaterial1;
+        for (i = (arrayOfMaterial1 = picks).length, b = 0; b < i; ) {
+            Material pick = arrayOfMaterial1[b];
+            if (mainHand.getType() == pick)
+                return true;
+            b++;
+        }
+        return false;
+    }
+
 
 }
